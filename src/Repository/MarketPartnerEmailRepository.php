fa8b617f616e82a4c472b7bde06d43e63c5e3289
Vlad <vlad.ginosyan@conuti.de>
2022-05-17 14:44:29 +0400

    [Feature][PDP-767@MPS]MarketPartnerEmail Repository Created

---
+++ src/Repository/MarketPartnerEmailRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\MarketPartnerEmail;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<MarketPartnerEmail>
+ *
+ * @method MarketPartnerEmail|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MarketPartnerEmail|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MarketPartnerEmail[]    findAll()
+ * @method MarketPartnerEmail[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MarketPartnerEmailRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MarketPartnerEmail::class);
+    }
+
+    public function add(MarketPartnerEmail $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(MarketPartnerEmail $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
