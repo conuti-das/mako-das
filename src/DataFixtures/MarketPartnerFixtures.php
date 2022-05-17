392b1bc187f19e17937227832f2b1404cc8e2dee
Nikolay Nikolov <nikolay.nikolov@conuti.de>
2022-05-17 14:44:42 +0200

    [PDP-767@MPS] Fix market partner fixtures

--- src/DataFixtures/MarketPartnerFixtures.php
+++ src/DataFixtures/MarketPartnerFixtures.php
@@ -1,20 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use App\Entity\MarketPartner;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use DateTime;
+use Exception;
 
 class MarketPartnerFixtures extends Fixture
 {
-
     /**
      * @param ObjectManager $manager
      *
      * @return void
-     * @throws \Exception
+     * @throws Exception
      */
     public function load(ObjectManager $manager): void
     {
@@ -23,15 +25,14 @@ class MarketPartnerFixtures extends Fixture
         $marketPartner->setCreatedAt(new DateTime(date("Y-m-d h:i:s")));
         $marketPartner->setType(['net', 'provider', 'msb', 'tso']);
         $marketPartner->setEnergy(['electricity', 'gas', 'water_sewage']);
-        $marketPartner->setPartnerId(1);
+        $marketPartner->setPartnerId('1');
         $marketPartner->setPartnerIdType(['bdew', 'iln', 'dvgw']);
-        $marketPartner->setPartnerIdGmsb(2);
+        $marketPartner->setPartnerIdGmsb('2');
         $marketPartner->setOrganization('testOrganization');
-        $marketPartner->setZip(2);
+        $marketPartner->setZip('76123');
         $marketPartner->setCity("Yerevan");
         $marketPartner->setStreet("Aram Xachatryan");
         $marketPartner->setHouseNumber("Aram Xachatryan 33");
-        $marketPartner->setAccountId(3);
         $marketPartner->setIban('testIban');
         $marketPartner->setBic('testBic');
         $marketPartner->setBank('bank');
@@ -47,8 +48,6 @@ class MarketPartnerFixtures extends Fixture
         $marketPartner->setAboDARef('test aboDARef');
         $marketPartner->setAboBgmNr('test AboBgmNr');
         $marketPartner->setAboSendDate(new DateTime(date("Y-m-d h:i:s")));
-        $marketPartner->setAboIsSent(2);
-        $marketPartner->setLockPayoutsRemadv(5);
         $marketPartner->setSyncNotWithMaster(2);
         $manager->persist($marketPartner);
         $manager->flush();
