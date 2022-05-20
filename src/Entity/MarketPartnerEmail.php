4a8f74d086186910714b4e62308b0d2abaec04f0
Nikolay Nikolov <nikolay.nikolov@conuti.de>
2022-05-20 12:11:14 +0200

    [PDP-766@MPS] Remove api tag

--- src/Entity/MarketPartnerEmail.php
+++ src/Entity/MarketPartnerEmail.php
@@ -10,7 +10,6 @@ use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MarketPartnerEmailRepository::class)]
-#[ApiResource]
 class MarketPartnerEmail
 {
     public const TYPE_EDIFACT = 'edifact';
