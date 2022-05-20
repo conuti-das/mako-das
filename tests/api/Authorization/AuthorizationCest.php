07ecd03fcda5752a8e0bb26b7ed296056328e92f
Nikolay Nikolov <nikolay.nikolov@conuti.de>
2022-05-20 16:48:39 +0200

    [PDP-766@MPS] Add the authorization success and failed tests

--- tests/api/Authorization/AuthorizationCest.php
+++ tests/api/Authorization/AuthorizationCest.php
@@ -8,7 +8,21 @@ use App\Tests\ApiTester;
 
 class AuthorizationCest
 {
-    public function authorizationTest(ApiTester $I): void {
-        $token = $I->getJWT();
+    public function authorizationFailedTest(ApiTester $I): void
+    {
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendGet('/api/market-partners');
+        $I->seeResponseCodeIs(401);
+        $I->assertJson(' {"code":401,"message":"JWT Token not found"}');
+    }
+
+    public function authorizationSuccessTest(ApiTester $I): void
+    {
+        $I->amBearerAuthenticated($I->getJWT());
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $I->sendGet('/api/market-partners');
+        $I->seeResponseCodeIs(200);
     }
 }
