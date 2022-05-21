14a683b01890754a478749f7459e6ca81ff31f51
Vlad <vlad.ginosyan@conuti.de>
2022-05-21 14:25:15 +0400

    [Feature][PDP-763@MPS]Created CertificateFormType

---
+++ src/Form/CertificateFormType.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class CertificateFormType
+ * @package App\Form
+ */
+class CertificateFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'partnerId',
+                TextType::class,
+                [
+                    'required' => true,
+                ]
+            )->add(
+                'uploadFile',
+                FileType::class,
+                [
+                    'required' => true,
+                ]
+            )->add(
+                'validFrom',
+                HiddenType::class,
+                [
+                    'required' => true,
+                ]
+            )->add(
+                'validUntil',
+                HiddenType::class,
+                [
+                    'required' => true,
+                ]
+            )->add(
+                'email',
+                HiddenType::class,
+                [
+                    'required' => true,
+                ]
+            )->add(
+                'certificateFile',
+                HiddenType::class,
+                [
+                    'required' => true,
+                ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
