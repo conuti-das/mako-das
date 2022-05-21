5f120726430812115eeaea39aeb3a2377f56e73b
Vlad <vlad.ginosyan@conuti.de>
2022-05-21 14:26:41 +0400

    [Feature][PDP-763@MPS]Added addCertificate method in MarketPartnerEmailRepository

--- src/Repository/MarketPartnerEmailRepository.php
+++ src/Repository/MarketPartnerEmailRepository.php
@@ -4,24 +4,40 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Dto\Certificate\UploadCertificateDto;
 use App\Entity\MarketPartnerEmail;
+use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 class MarketPartnerEmailRepository extends ServiceEntityRepository
 {
+    /**
+     * @param ManagerRegistry $registry
+     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MarketPartnerEmail::class);
     }
 
-    public function add(MarketPartnerEmail $entity, bool $flush = false): void
+    /**
+     * @param UploadCertificateDto $uploadCertificateDto
+     *
+     * @return void
+     */
+    public function addCertificate(UploadCertificateDto $uploadCertificateDto): void
     {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
+        $marketPartnerEmail = new MarketPartnerEmail();
+        $marketPartnerEmail->setMarketPartnerId($uploadCertificateDto->getPartnerId());
+        $marketPartnerEmail->setCreatedAt(new DateTime('now'));
+        $marketPartnerEmail->setEmail($uploadCertificateDto->getEmailAddress());
+        $marketPartnerEmail->setType('edifact');
+        $marketPartnerEmail->setSslCertificate($uploadCertificateDto->getCertificateFile());
+        $marketPartnerEmail->setSslCertificateExpiration($uploadCertificateDto->getValidUntil());
+        $marketPartnerEmail->setActiveFrom($uploadCertificateDto->getValidUntil());
+        $marketPartnerEmail->setActiveUntil($uploadCertificateDto->getValidFrom());
+        $this->getEntityManager()->persist($marketPartnerEmail);
+        $this->getEntityManager()->flush();
     }
 
     public function remove(MarketPartnerEmail $entity, bool $flush = false): void
