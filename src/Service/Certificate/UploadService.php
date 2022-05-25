2b25d9cfee9316f461b0c847432afcd40d499a20
Vlad <vlad.ginosyan@conuti.de>
2022-05-25 16:49:49 +0400

    [Feature][PDP-763@MPS]Added try catch

--- src/Service/Certificate/UploadService.php
+++ src/Service/Certificate/UploadService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service\Certificate;
 
 use App\Exception\Certificate\CertificateParseException;
+use App\Exception\Certificate\UploadCertificateException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Exception;
 
@@ -16,17 +17,23 @@ class UploadService
      *
      * @return string
      * @throws CertificateParseException
+     * @throws UploadCertificateException
      */
     public function upload(UploadedFile $file, string $directoryName): string
     {
         try {
             $fileName = $this->generateName($file->getClientOriginalName());
             $file->move($directoryName, $fileName);
+        } catch (Exception $exception) {
+            throw new UploadCertificateException('Given certificate could not be uploaded.');
+        }
+
+        try {
             $uploadedCertificate = file_get_contents($directoryName . '/' . $fileName);
 
             return $uploadedCertificate;
         } catch (Exception $exception) {
-            throw new CertificateParseException('Given certificate could not be uploaded.');
+            throw new CertificateParseException('Given certificate could not be parsed.');
         }
     }
 
