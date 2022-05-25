c2803af5d3051293f992a266617df50cb0828bb6
Vlad <vlad.ginosyan@conuti.de>
2022-05-25 16:48:33 +0400

    [Feature][PDP-763@MPS]removed the doc block use the flush

--- src/Repository/MarketPartnerEmailRepository.php
+++ src/Repository/MarketPartnerEmailRepository.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use App\Dto\Certificate\UploadCertificateDto;
 use App\Entity\MarketPartnerEmail;
 use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -12,32 +11,28 @@ use Doctrine\Persistence\ManagerRegistry;
 
 class MarketPartnerEmailRepository extends ServiceEntityRepository
 {
-    /**
-     * @param ManagerRegistry $registry
-     */
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MarketPartnerEmail::class);
     }
 
-    /**
-     * @param UploadCertificateDto $uploadCertificateDto
-     *
-     * @return void
-     */
-    public function addCertificate(UploadCertificateDto $uploadCertificateDto): void
+    public function addCertificate($entity, $uploadCertificateDto, bool $flush = false)
     {
-        $marketPartnerEmail = new MarketPartnerEmail();
-        $marketPartnerEmail->setMarketPartnerId($uploadCertificateDto->getPartnerId());
-        $marketPartnerEmail->setCreatedAt(new DateTime('now'));
-        $marketPartnerEmail->setEmail($uploadCertificateDto->getEmailAddress());
-        $marketPartnerEmail->setType('edifact');
-        $marketPartnerEmail->setSslCertificate($uploadCertificateDto->getCertificateFile());
-        $marketPartnerEmail->setSslCertificateExpiration($uploadCertificateDto->getValidUntil());
-        $marketPartnerEmail->setActiveFrom($uploadCertificateDto->getValidUntil());
-        $marketPartnerEmail->setActiveUntil($uploadCertificateDto->getValidFrom());
-        $this->getEntityManager()->persist($marketPartnerEmail);
-        $this->getEntityManager()->flush();
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $entity->setMarketPartnerId($uploadCertificateDto->getPartnerId());
+            $entity->setCreatedAt(new DateTime('now'));
+            $entity->setEmail($uploadCertificateDto->getEmailAddress());
+            $entity->setType($entity::TYPE_EDIFACT);
+            $entity->setSslCertificate($uploadCertificateDto->getCertificateFile());
+            $entity->setSslCertificateExpiration($uploadCertificateDto->getValidUntil());
+            $entity->setActiveFrom($uploadCertificateDto->getValidUntil());
+            $entity->setActiveUntil($uploadCertificateDto->getValidFrom());
+            $this->getEntityManager()->flush();
+
+            return $entity;
+        }
     }
 
     public function remove(MarketPartnerEmail $entity, bool $flush = false): void
