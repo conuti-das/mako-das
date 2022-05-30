2e78cec3f4558c0eb85e2645486b877c915422cc
Vlad <vlad.ginosyan@conuti.de>
2022-05-30 12:51:39 +0400

    [Feature][PDP-762@MPS]created migration

---
+++ migrations/Version20220530082709.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220530082709 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE market_partner_email ADD CONSTRAINT FK_6540CD20FD6DACE2 FOREIGN KEY (market_partner_id) REFERENCES market_partner (id)');
+        $this->addSql('CREATE INDEX IDX_6540CD20FD6DACE2 ON market_partner_email (market_partner_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE market_partner_email DROP FOREIGN KEY FK_6540CD20FD6DACE2');
+        $this->addSql('DROP INDEX IDX_6540CD20FD6DACE2 ON market_partner_email');
+    }
+}
