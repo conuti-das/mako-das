280d91c1436176cbecca28d1275f0c96266d8ddc
Nikolay Nikolov <nikolay.nikolov@conuti.de>
2022-06-01 12:41:11 +0200

    [PDP-839@MPS] Fix the unit test

--- tests/unit/Service/Certificate/CertificateServiceTest.php
+++ tests/unit/Service/Certificate/CertificateServiceTest.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace App\Tests\unit\Service\Certificate;
 
 use App\Exception\Certificate\CertificateEmptyException;
+use App\Exception\Certificate\CertificateParseException;
 use App\Exception\Certificate\CertificateReadException;
 use App\Service\Certificate\CertificateService;
 use App\Tests\UnitTester;
 use Codeception\Test\Unit;
 use DateTime;
+use JsonException;
 
 class CertificateServiceTest extends Unit
 {
     protected UnitTester $tester;
     private string $certificate;
 
-    protected function _before()
+    protected function _before(): void
     {
         $this->certificate = file_get_contents(codecept_data_dir() . 'Certificate/9911620000000.cer');
     }
@@ -77,6 +79,12 @@ class CertificateServiceTest extends Unit
         $this->tester->assertFalse($certificateDto->isActive());
     }
 
+    /**
+     * @throws JsonException
+     * @throws CertificateEmptyException
+     * @throws CertificateParseException
+     * @throws CertificateReadException
+     */
     public function testJsonRepresentation(): void
     {
         $nowDate = new DateTime('2022-05-18 14:55:55');
@@ -91,11 +99,15 @@ class CertificateServiceTest extends Unit
         $certificateDto->setValidUntil($newActiveUntil->modify('+2 months'));
 
         $this->tester->assertJsonStringEqualsJsonString(
-            '{"name":"\/emailAddress=bes_vertrieb@bigge-energie.de\/CN=bes_vertrieb\/O=BIGGE Energie GmbH & Co. KG\/L=Attendorn\/C=DE","hash":"6b7f2420","serialNumber":"0x056C0E37A7984B3DC3AB73EB3411988D49C2460A","emailAddress":"bes_vertrieb@bigge-energie.de","validFrom":{"date":"2022-03-18 14:55:55.000000","timezone_type":3,"timezone":"UTC"},"validUntil":{"date":"2022-07-18 14:55:55.000000","timezone_type":3,"timezone":"UTC"},"subjectName":"bes_vertrieb","subjectOrganisation":"BIGGE Energie GmbH & Co. KG","subjectLocation":"Attendorn","subjectCountry":"DE","issuerName":"procilon GROUP Customer CA - EDIFACT 03","issuerOrganisation":"SPI-CLOUD","issuerOrganisationUnit":"Sub CA","issuerCountry":"DE","isActive":true}',
+            '{"name":"\/emailAddress=bes_vertrieb@bigge-energie.de\/CN=bes_vertrieb\/O=BIGGE Energie GmbH & Co. KG\/L=Attendorn\/C=DE","hash":"6b7f2420","serialNumber":"0x056C0E37A7984B3DC3AB73EB3411988D49C2460A","emailAddress":"bes_vertrieb@bigge-energie.de","validFrom":{"date":"2022-03-18 14:55:55.000000","timezone_type":3,"timezone":"UTC"},"validUntil":{"date":"2022-07-18 14:55:55.000000","timezone_type":3,"timezone":"UTC"},"subjectName":"bes_vertrieb","subjectOrganisation":"BIGGE Energie GmbH & Co. KG","subjectLocation":"Attendorn","subjectCountry":"DE","issuerName":"procilon GROUP Customer CA - EDIFACT 03","issuerOrganisation":"SPI-CLOUD","issuerOrganisationUnit":"Sub CA","issuerCountry":"DE","certificateFile":"-----BEGIN CERTIFICATE-----\r\nMIIHSjCCBP6gAwIBAgIUBWwON6eYSz3Dq3PrNBGYjUnCRgowQQYJKoZIhvcNAQEK\r\nMDSgDzANBglghkgBZQMEAgMFAKEcMBoGCSqGSIb3DQEBCDANBglghkgBZQMEAgMF\r\nAKIDAgFAMGQxMDAuBgNVBAMMJ3Byb2NpbG9uIEdST1VQIEN1c3RvbWVyIENBIC0g\r\nRURJRkFDVCAwMzEPMA0GA1UECwwGU3ViIENBMRIwEAYDVQQKDAlTUEktQ0xPVUQx\r\nCzAJBgNVBAYTAkRFMB4XDTIyMDQxNDE0MzI0NloXDTI1MDQxMzE0MzI0NVowgYwx\r\nLDAqBgkqhkiG9w0BCQEWHWJlc192ZXJ0cmllYkBiaWdnZS1lbmVyZ2llLmRlMRUw\r\nEwYDVQQDDAxiZXNfdmVydHJpZWIxJDAiBgNVBAoMG0JJR0dFIEVuZXJnaWUgR21i\r\nSCAmIENvLiBLRzESMBAGA1UEBwwJQXR0ZW5kb3JuMQswCQYDVQQGEwJERTCCAiIw\r\nDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAMUKUWcLYFdBYasnmEtrw31AHmHS\r\nYXY16qt\/w6PmDk26aa1D4lv3UZG0IwFyNKEtJnRt4zd9FqXRM0xJug6FHSzJaPTH\r\nA1WhWwMgXfdm4HmyP87Pm7W4ythxbXsELUZnvVg2WJObU1E0TAD7fDwM+jT2zlp8\r\nhf3gBgT5q9yjEi\/Gn34LqS\/\/BsdULLxolRJOd410sdg9moQ4uwubKMlFFgOVkHyz\r\nTsE2XbAyJda7TFY9JofJsKS9uCwaKebYJRQaarGytEbgVpN9rshiDM6NdTUoffg5\r\ne+I5futkT2zWOc7F45UgpD3+kvvoMOeZOAMhMHpiekTU6w3AQBEI8925wtD0B4Nm\r\ne+DkQqZSBPO5MuYr\/ifqgXSnNEcha0262LfNzmC94ZDXdwoa5le8FS\/ob6plQGl4\r\nYQwlYKsR6EArX5f9k7jnVMB\/2LXp\/DY4HElsRZJak3awgFRoG7qBmpugiBzsghWb\r\nBMHSNlarqQJ78n9WBx1x350ddji\/bEAT5mMkZMuzfyQxcOxNIkQSezmP1DPQ3Q4K\r\nNFK2aQ1um1V3bXAPLzYK0PcuKP1upGG5gWAawtwcgBP7DLZfToTo8+WvtM6wofOk\r\n5QQm9rvb0zF2yKHmR81M2NJQZOyrHjzo7g8T2yT+ExquzPuy+1kKWx5DEd\/7yJJs\r\nemCE\/PeGuXe1x00xAgMBAAGjggFhMIIBXTAMBgNVHRMBAf8EAjAAMB8GA1UdIwQY\r\nMBaAFERE75jnFOhN1pLIH\/nzZ9bk+BmpMGkGCCsGAQUFBwEBBF0wWzAqBggrBgEF\r\nBQcwAoYeaHR0cHM6Ly9wa2kucHJvdGVjdHIuZGUvaXNzdWVyMC0GCCsGAQUFBzAB\r\nhiFodHRwOi8vb2NzcC5zcGktY2xvdWQuY29tL3N0YXR1cy8wKAYDVR0RBCEwH4Ed\r\nYmVzX3ZlcnRyaWViQGJpZ2dlLWVuZXJnaWUuZGUwEwYDVR0lBAwwCgYIKwYBBQUH\r\nAwQwUwYDVR0fBEwwSjBIoEagRIZCaHR0cDovL3BraS5zcGktY2xvdWQuY29tL2Ny\r\nbC9wcm9jaWxvbl9HUk9VUF9DdXN0b21lcl9DQV9FRElGQUNUXzAzMB0GA1UdDgQW\r\nBBReLTw33bjrn89CGniYvQvh\/b7YezAOBgNVHQ8BAf8EBAMCBLAwQQYJKoZIhvcN\r\nAQEKMDSgDzANBglghkgBZQMEAgMFAKEcMBoGCSqGSIb3DQEBCDANBglghkgBZQME\r\nAgMFAKIDAgFAA4ICAQBOJFv8Ocxuu2D9J+C\/sa4gA45ZE1UsfWRX1g6qQkkTxAhQ\r\nlZZbVAf0mT\/pkArYWG5LaUIJqksuyPwWT7d40GuVmUmTWM3bD98dtQ71NpMENhga\r\nK\/WTVSBFIm0VVam2DnbSJJ40IYXooQ9Sj6KtHtSG6XmTap1Wo6+hLDphcJd65NI0\r\n9KaM0kOpQBaS0uKTe2d83FvFVKBzIRJd+ue0cDlUSlm7pwbUEkHaQM3MTAJB6tgG\r\nRFw8GXCjyvxBrgghp9DZJ0vkT68udZNpSYE1L4bLWkpEUVQYbogRgRimi6NLXcP7\r\nIA2xWFV5O7iOZ++JSQr1eOAEZY1qVEv20HRbHLjlmVKUC3XgPlCSFmejH3JOjTg2\r\n4XZmXedRXiWsXLGRicu7bsE47IZm83f0JyNZBj+4VMMKuX14t+5PgySjMm7wByKF\r\n+VrTYuDxNlMOH5piVVfcKYOjpoGIIz0RGxmLlUS6oryLLoq8dO7xg\/zcZMtzq\/JN\r\nWDkVC3D8ldIzHOGhdiIvwgPfL0s\/DCMLulU79237Z7uAbckE8Xdy2YPn7if2tDHx\r\ncAZlFlhVwQlerdu9V\/+HUcxxAIqyYq1dcbH2FTo0pbEbfi0kWSAvnX9nhTs0WBVK\r\nTQx6raD5Z2oxm\/1b7FTFcFoCu5W8bKRlqPFStTnvdIpdBldcUQElWphePG6MfQ==\r\n-----END CERTIFICATE-----\r\n","isActive":true}',
             $certificateDto->toJson()
         );
     }
 
+    /**
+     * @throws CertificateReadException
+     * @throws CertificateParseException
+     */
     public function testCertificateEmptyException(): void
     {
         $this->expectException(CertificateEmptyException::class);
@@ -104,6 +116,10 @@ class CertificateServiceTest extends Unit
         $certificateService->decode('');
     }
 
+    /**
+     * @throws CertificateEmptyException
+     * @throws CertificateParseException
+     */
     public function testCertificateReadException(): void
     {
         $this->expectException(CertificateReadException::class);
