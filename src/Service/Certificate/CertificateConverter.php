7aeae65f49a8d5158016dfc280b15cc84a6a1313
Nikolay Nikolov <nikolay.nikolov@conuti.de>
2022-06-01 14:57:27 +0200

    [PDP-839@MPS] Add Certificate format converter

--- src/Service/Certificate/CertificateConverter.php
+++ src/Service/Certificate/CertificateConverter.php
@@ -4,33 +4,17 @@ declare(strict_types=1);
 
 namespace App\Service\Certificate;
 
-use App\Exception\Certificate\CertificateWrongFormatException;
-
 class CertificateConverter
 {
-    /**
-     * @throws CertificateWrongFormatException
-     */
     public function convertDER2PEM(string $derData): string
     {
-        if (!$this->isDER($derData)) {
-            throw new CertificateWrongFormatException('Given certificate is not in DER format.');
-        }
-
         $pem = chunk_split(base64_encode($derData), 64, "\n");
 
         return "-----BEGIN CERTIFICATE-----\n" . $pem . "-----END CERTIFICATE-----\n";
     }
 
-    /**
-     * @throws CertificateWrongFormatException
-     */
     public function convertPEM2DER(string $pemData): string
     {
-        if (!$this->isPEM($pemData)) {
-            throw new CertificateWrongFormatException('Given certificate is not in PEM format.');
-        }
-
         $begin = "CERTIFICATE-----";
         $end = "-----END";
         $pemData = substr($pemData, strpos($pemData, $begin) + strlen($begin));
@@ -47,9 +31,9 @@ class CertificateConverter
 
     public function isDER(string $certificate): bool
     {
-        $certificate = base64_encode($certificate);
+        $convertedToPEM = $this->convertDER2PEM($certificate);
+        $convertedToDER = $this->convertPEM2DER($convertedToPEM);
 
-        return str_contains($certificate, 'CERTIFICATE-----')
-            && str_contains($certificate, '-----END');
+        return md5($certificate) === md5($convertedToDER);
     }
 }
