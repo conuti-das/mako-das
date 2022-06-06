c70d5e561074c81c9f95ed8d6960cab6b89aba01
rafayel <rafayel.datvyan@conuti.de>
2022-06-06 19:54:33 +0400

    [Feature][PDP-768@MPS]cron job

--- src/Command/ImportMarketPartnerCommand.php
+++ src/Command/ImportMarketPartnerCommand.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace App\Command;
 
+use App\Common\FileReader;
 use App\Service\Import\ImportMarketPartnerService;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 #[AsCommand(
     name: 'market-partner:import',
@@ -16,14 +18,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class ImportMarketPartnerCommand extends Command
 {
-    public function __construct(private ImportMarketPartnerService $importMarketPartnerService)
-    {
+    const MARKET_PARTNER_DATA_FILE_NAME = "market_partner_for_certificates_postman.csv";
+
+    public function __construct(
+        private ImportMarketPartnerService $importMarketPartnerService,
+        private ParameterBagInterface $parameterBag,
+        private FileReader $fileReader
+    ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->importMarketPartnerService->importFromCsv();
+        $marketPartners = $this->fileReader->csvToArray(
+            $this->parameterBag->get('importCertificateDirectory') . '/' . self::MARKET_PARTNER_DATA_FILE_NAME
+        );
+
+        foreach ($marketPartners as $marketPartner) {
+            $this->importMarketPartnerService->import($marketPartner);
+        }
+
         return Command::SUCCESS;
     }
 }
