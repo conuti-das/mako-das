dc2174f396e89ba8a5249768f3b741c50a270b11
rafayel <rafayel.datvyan@conuti.de>
2022-06-06 19:55:20 +0400

    [Feature][PDP-768@MPS]created fileReader

---
+++ src/Common/FileReader.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Common;
+
+use App\Exception\Certificate\CertificateParseException;
+use Exception;
+
+class FileReader
+{
+    public function getContent(string $filePath): string
+    {
+        try {
+            return file_get_contents($filePath);
+        } catch (Exception $exception) {
+            throw new CertificateParseException("Can not read file, in FileReader::getContent function. ${$filePath}");
+        }
+    }
+
+    public function csvToArray($csvFile, $length = 1000, $separator = ','): array
+    {
+        $lines = [];
+
+        try {
+            $fileToRead = fopen($csvFile, 'r');
+            $index = fgetcsv($fileToRead, $length, $separator);
+
+            while (!feof($fileToRead)) {
+                $listParams = fgetcsv($fileToRead, $length, $separator);
+                if ($listParams) {
+                    $lines[] = array_combine($index, $listParams);
+                }
+            }
+            fclose($fileToRead);
+        } catch (Exception $exception) {
+            throw new Exception("Invalid csv provided in FileReader::csvToArray function. ${$csvFile}");
+        }
+
+        return $lines;
+    }
+}
