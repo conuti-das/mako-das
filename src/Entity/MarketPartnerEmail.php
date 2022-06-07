55119383228d3c5b61d48bf0747e8acc9e5315a8
rafayel <rafayel.datvyan@conuti.de>
2022-06-07 15:54:06 +0400

    [Feature][PDP-768@MPS] added OneToMany for emails

--- src/Entity/MarketPartnerEmail.php
+++ src/Entity/MarketPartnerEmail.php
@@ -6,6 +6,8 @@ namespace App\Entity;
 
 use App\Repository\MarketPartnerEmailRepository;
 use DateTimeInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MarketPartnerEmailRepository::class)]
@@ -43,6 +45,15 @@ class MarketPartnerEmail
     #[ORM\ManyToOne(targetEntity: MarketPartner::class, inversedBy: 'marketPartnerEmails')]
     #[ORM\JoinColumn(nullable: false)]
     private MarketPartner $marketPartner;
+
+    #[ORM\OneToMany(mappedBy: 'marketPartnerEmail', targetEntity: MarketPartnerEmailImportLog::class)]
+    private $marketPartnerEmailImportLogs;
+
+    public function __construct()
+    {
+        $this->marketPartnerEmailImportLogs = new ArrayCollection();
+    }
+
 
     public function getId(): int
     {
@@ -148,6 +159,32 @@ class MarketPartnerEmail
     public function setMarketPartner(MarketPartner $marketPartner): self
     {
         $this->marketPartner = $marketPartner;
+
+        return $this;
+    }
+
+    public function getMarketPartnerEmailImportLogs(): Collection
+    {
+        return $this->marketPartnerEmailImportLogs;
+    }
+
+    public function addMarketPartnerEmailImportLog(MarketPartnerEmailImportLog $marketPartnerEmailImportLog): self
+    {
+        if (!$this->marketPartnerEmailImportLogs->contains($marketPartnerEmailImportLog)) {
+            $this->marketPartnerEmailImportLogs[] = $marketPartnerEmailImportLog;
+            $marketPartnerEmailImportLog->setMarketPartnerEmail($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMarketPartnerEmailImportLog(MarketPartnerEmailImportLog $marketPartnerEmailImportLog): self
+    {
+        if ($this->marketPartnerEmailImportLogs->removeElement($marketPartnerEmailImportLog)) {
+            if ($marketPartnerEmailImportLog->getMarketPartnerEmail() === $this) {
+                $marketPartnerEmailImportLog->setMarketPartnerEmail(null);
+            }
+        }
 
         return $this;
     }
