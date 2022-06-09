4a9110d0176b3f9ec6bf9c71bcb318bad72fec60
Vlad <vlad.ginosyan@conuti.de>
2022-06-10 00:36:25 +0400

    [Feature][PDP-848@MPS]create and delete User

--- tests/api/Authorization/AuthorizationCest.php
+++ tests/api/Authorization/AuthorizationCest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\api\Authorization;
 
 use App\Tests\ApiTester;
+use App\Tests\Faker\FakerUser;
 
 class AuthorizationCest
 {
@@ -19,10 +20,13 @@ class AuthorizationCest
 
     public function authorizationSuccessTest(ApiTester $I): void
     {
+        $fakerUser = new FakerUser();
+        $fakerUser->create([]);
         $I->amBearerAuthenticated($I->getJWT());
         $I->haveHttpHeader('accept', 'application/json');
         $I->haveHttpHeader('Content-Type', 'application/json');
         $I->sendGet('/api/market-partners');
         $I->seeResponseCodeIs(200);
+        $fakerUser->delete();
     }
 }
