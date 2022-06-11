7dfba89718dc5c38276a8ab1254825182ea2243b
Vlad <vlad.ginosyan@conuti.de>
2022-06-11 16:10:14 +0400

    [Feature][PDP-848@MPS]created ApiTest.php file

---
+++ tests/api/ApiTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\api;
+
+use App\Entity\MarketPartner;
+use App\Entity\MarketPartnerEmail;
+use App\Entity\User;
+use App\Tests\ApiTester;
+
+class ApiTest
+{
+    protected User $apiUser;
+    protected MarketPartner $apiMarketPartner;
+    protected MarketPartnerEmail $apiMarketPartnerEmail;
+
+    public function _before(ApiTester $I): void
+    {
+        $this->apiUser = $I->createApiUser();
+        $this->apiMarketPartner = $I->createApiMarketPartner();
+        $this->apiMarketPartnerEmail = $I->createApiMarketPartnerEmail();
+    }
+
+    public function _after(ApiTester $I): void
+    {
+        $I->deleteAPIUser($this->apiUser);
+        $I->deleteAPIMarketPartnerEmail($this->apiMarketPartnerEmail);
+        $I->deleteAPIMarketPartner($this->apiMarketPartner);
+    }
+}
