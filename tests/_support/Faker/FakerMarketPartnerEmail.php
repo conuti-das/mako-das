b856c22d0b83343486686329e84c01039fc274ab
Vlad <vlad.ginosyan@conuti.de>
2022-06-11 16:02:28 +0400

    [Feature][PDP-848@MPS]created FakerMarketPartnerEmail file for delete and create

---
+++ tests/_support/Faker/FakerMarketPartnerEmail.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Faker;
+
+use App\Entity\MarketPartner;
+use App\Entity\MarketPartnerEmail;
+use App\Tests\Exception\MarketPartner\MarketPartnerEmptyException;
+use DateTime;
+
+class FakerMarketPartnerEmail extends Faker
+{
+    public function create(?array $data): MarketPartnerEmail
+    {
+        $marketPartner = $this->entityManager->getRepository(MarketPartner::class)->findAll();
+
+        if (!isset($marketPartner[0])) {
+            throw new MarketPartnerEmptyException("marketPartner not found");
+        }
+
+        $nowDate = new DateTime('now');
+        $expiredDate = clone $nowDate->modify('+1 year');
+
+        $marketPartnerEmail = new MarketPartnerEmail();
+        $marketPartnerEmail->setId($data['id'] ?? 1);
+        $marketPartnerEmail->setMarketPartner($data['marketPartner'] ?? $marketPartner[0]);
+        $marketPartnerEmail->setCreatedAt($data['createdAt'] ?? $nowDate);
+        $marketPartnerEmail->setUpdatedAt($data['updatedAt'] ?? $nowDate);
+        $marketPartnerEmail->setEmail($data['email'] ?? 'debug@conuti.de');
+        $marketPartnerEmail->setType($data['type'] ?? MarketPartnerEmail::TYPE_EDIFACT);
+        $marketPartnerEmail->setSslCertificate(
+            $data['sslCertificate'] ?? 'rjh$ZaG+xbHUauc2p5PvwNhReQ==.fdd873cc2c87b844f972a4bfa086cbb0'
+        );
+        $marketPartnerEmail->setSslCertificateExpiration($data['sslCertificateExpiration'] ?? $expiredDate);
+        $marketPartnerEmail->setActiveFrom($data['activeFrom'] ?? $nowDate);
+        $marketPartnerEmail->setActiveUntil($data['activeUntil'] ?? $expiredDate);
+
+        $this->entityManager->persist($marketPartnerEmail);
+        $this->entityManager->flush();
+
+        return $marketPartnerEmail;
+    }
+
+    public function delete(mixed $object): void
+    {
+        $this->entityManager->remove($object);
+        $this->entityManager->flush();
+    }
+}
