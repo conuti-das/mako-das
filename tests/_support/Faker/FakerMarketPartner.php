69dfc3c0dfa2885b6cdeb21b33454a23bffa7826
Vlad <vlad.ginosyan@conuti.de>
2022-06-11 18:21:00 +0400

    [Feature][PDP-848@MPS]created FakerMarketPartner

--- tests/_support/Faker/FakerMarketPartner.php
+++ tests/_support/Faker/FakerMarketPartner.php
@@ -6,56 +6,47 @@ namespace App\Tests\Faker;
 
 use App\Entity\MarketPartner;
 use DateTime;
-use Doctrine\ORM\Id\AssignedGenerator;
-use Doctrine\ORM\Mapping\ClassMetadataInfo;
 
 class FakerMarketPartner extends Faker
 {
     public function create(?array $data): MarketPartner
     {
-        $entityManager = parent::getContainer()->get('doctrine.orm.entity_manager');
         $marketPartner = new MarketPartner();
-        $marketPartner->setId(1);
-        $marketPartner->setActive(1);
-        $marketPartner->setDeleted(0);
-        $marketPartner->setCreatedAt(new DateTime('now'));
-        $marketPartner->setType(MarketPartner::TYPE_NET);
-        $marketPartner->setEnergy(MarketPartner::ENERGY_ELECTRICITY);
-        $marketPartner->setPartnerId('9900080000007');
-        $marketPartner->setPartnerIdType(MarketPartner::NUMBER_BDEW);
-        $marketPartner->setOrganization('Stromnetz Berlin GmbH');
-        $marketPartner->setZip('12435');
-        $marketPartner->setCity("Berlin");
-        $marketPartner->setStreet("Eichenstraße");
-        $marketPartner->setHouseNumber("3a");
-        $marketPartner->setIban('DE85120300000001122445');
-        $marketPartner->setBic('BYLADEM1001');
-        $marketPartner->setBank('DEUTSCHE KREDIT BANK A.G. BERLIN');
-        $marketPartner->setAccountHolder('Stromnetz Berlin GmbH');
-        $marketPartner->setPhone('+49 30 0000 000');
-        $marketPartner->setRegisterCourt('Charlottenburg (Berlin) HRB 179968 B');
-        $marketPartner->setRegisterNumber('DE308492655');
-        $marketPartner->setSign(0);
-        $marketPartner->setCompress(0);
-        $marketPartner->setEncrypt(0);
-        $marketPartner->setReminderEmailAddress('debug@conuti.de');
-        $marketPartner->setUsingTumCatalog(0);
-
-        $metadata = $entityManager->getClassMetaData(MarketPartner::class);
-        $metadata->setIdGenerator(new AssignedGenerator());
-        $metadata->setIdGeneratorType(ClassMetadataInfo::GENERATOR_TYPE_NONE);
-        $entityManager->persist($marketPartner);
-        $entityManager->flush();
+        $marketPartner->setId($data['id'] ?? 1);
+        $marketPartner->setActive($data['active'] ?? 1);
+        $marketPartner->setDeleted($data['deletedAt'] ?? 0);
+        $marketPartner->setCreatedAt($data['createdAt'] ?? new DateTime("2022-05-23T13:17:20+00:00"));
+        $marketPartner->setUpdatedAt($data['updatedAt'] ?? new DateTime("2022-06-23T00:00:00+00:00"));
+        $marketPartner->setType($data['type'] ?? MarketPartner::TYPE_NET);
+        $marketPartner->setEnergy($data['energy'] ?? MarketPartner::ENERGY_ELECTRICITY);
+        $marketPartner->setPartnerId($data['partnerId'] ?? '9900080000007');
+        $marketPartner->setPartnerIdType($data['partnerIdType'] ?? MarketPartner::NUMBER_BDEW);
+        $marketPartner->setOrganization($data['organization'] ?? 'Stromnetz Berlin GmbH');
+        $marketPartner->setZip($data['zip'] ?? '12435');
+        $marketPartner->setCity($data['city'] ?? "Berlin");
+        $marketPartner->setStreet($data['street'] ?? "Eichenstraße");
+        $marketPartner->setHouseNumber($data['houseNumber'] ?? "3a");
+        $marketPartner->setIban($data['iban'] ?? 'DE85120300000001122445');
+        $marketPartner->setBic($data['bic'] ?? 'BYLADEM1001');
+        $marketPartner->setBank($data['bank'] ?? 'DEUTSCHE KREDIT BANK A.G. BERLIN');
+        $marketPartner->setAccountHolder($data['accountHolder'] ?? 'Stromnetz Berlin GmbH');
+        $marketPartner->setPhone($data['phone'] ?? '+49 30 0000 000');
+        $marketPartner->setRegisterCourt($data['registerCourt'] ?? 'Charlottenburg (Berlin) HRB 179968 B');
+        $marketPartner->setRegisterNumber($data['registerNumber'] ?? 'DE308492655');
+        $marketPartner->setSign($data['sign'] ?? 0);
+        $marketPartner->setCompress($data['compress'] ?? 0);
+        $marketPartner->setEncrypt($data['encrypt'] ?? 0);
+        $marketPartner->setReminderEmailAddress($data['reminderEmailAddress'] ?? 'debug@conuti.de');
+        $marketPartner->setUsingTumCatalog($data['usingTumCatalog'] ?? 0);
+        $this->entityManager->persist($marketPartner);
+        $this->entityManager->flush();
 
         return $marketPartner;
     }
 
-    public function delete(): void
+    public function delete(mixed $object): void
     {
-        $entityManager = parent::getContainer()->get('doctrine.orm.entity_manager');
-        $connection = $entityManager->getConnection();
-        $platform = $connection->getDatabasePlatform();
-        $tableName = $entityManager->getClassMetadata(MarketPartner::class)->getTableName();
-        $connection->executeUpdate($platform->getTruncateTableSQL($tableName, true));
+        $this->entityManager->remove($object);
+        $this->entityManager->flush();
     }
 }
