11e880914ec7ce69391b137deaadfee37fe3ec80
Vlad <vlad.ginosyan@conuti.de>
2022-06-11 16:04:04 +0400

    [Feature][PDP-848@MPS]added createApi and deleteApi methodes

--- tests/_support/ApiTester.php
+++ tests/_support/ApiTester.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Entity\MarketPartner;
+use App\Entity\MarketPartnerEmail;
+use App\Entity\User;
+use App\Tests\Faker\FakerMarketPartner;
+use App\Tests\Faker\FakerMarketPartnerEmail;
+use App\Tests\Faker\FakerUser;
 use Codeception\Actor;
 
 /**
@@ -42,5 +48,59 @@ class ApiTester extends Actor
         $this->assertNotEmpty($token);
 
         return $token[0];
+    }
+
+    public function createApiUser(): User
+    {
+        /** @var FakerUser $fakerUser */
+        $fakerUser = $this->grabService(FakerUser::class);
+
+        return $fakerUser->create([
+            'username' => $_ENV['API_TEST_USERNAME'],
+            'password' => $_ENV['API_TEST_PASSWORD'],
+            'roles' => ["ROLE_USER_API"]
+        ]);
+    }
+
+    public function createApiMarketPartner(): MarketPartner
+    {
+        /** @var FakerMarketPartner $fakerMarketPartner */
+        $fakerMarketPartner = $this->grabService(FakerMarketPartner::class);
+
+        return $fakerMarketPartner->create([
+            'type' => MarketPartner::TYPE_NET,
+            'energy' => MarketPartner::ENERGY_ELECTRICITY,
+            'partnerIdType' => MarketPartner::NUMBER_BDEW,
+        ]);
+    }
+
+    public function createApiMarketPartnerEmail(): MarketPartnerEmail
+    {
+        /** @var FakerMarketPartnerEmail $fakerMarketPartnerEmail */
+        $fakerMarketPartnerEmail = $this->grabService(FakerMarketPartnerEmail::class);
+
+        return $fakerMarketPartnerEmail->create([
+        ]);
+    }
+
+    public function deleteAPIUser(User $user): void
+    {
+        /** @var FakerUser $fakerUser */
+        $fakerUser = $this->grabService(FakerUser::class);
+        $fakerUser->delete($user);
+    }
+
+    public function deleteAPIMarketPartner(MarketPartner $marketPartner): void
+    {
+        /** @var FakerMarketPartner $fakerMarketPartner */
+        $fakerMarketPartner = $this->grabService(FakerMarketPartner::class);
+        $fakerMarketPartner->delete($marketPartner);
+    }
+
+    public function deleteAPIMarketPartnerEmail(MarketPartnerEmail $marketPartnerEmail): void
+    {
+        /** @var FakerMarketPartnerEmail $fakerMarketPartnerEmail */
+        $fakerMarketPartnerEmail = $this->grabService(FakerMarketPartnerEmail::class);
+        $fakerMarketPartnerEmail->delete($marketPartnerEmail);
     }
 }
